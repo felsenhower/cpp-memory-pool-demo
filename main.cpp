61c6191ab513d9e439dcaa93db36fd03b11931b1
felsenhower <4felgenh@informatik.uni-hamburg.de>
2019-05-06 15:18:12 +0200

    Remove static variable

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-static struct {
+struct mempool {
     char *mem;
     size_t offset = 0;
     size_t size;
-} memory_pool;
+};
 
 template <class T>
 struct Mallocator {
@@ -17,6 +17,12 @@ struct Mallocator {
     typedef T value_type;
     
     Mallocator() = default;
+    
+    mempool *memory_pool;    
+    
+    Mallocator(mempool *memory_pool) {
+        this->memory_pool = memory_pool;
+    }
     
     template <class U> constexpr Mallocator(const Mallocator<U>&) noexcept {}
     
@@ -26,11 +32,11 @@ struct Mallocator {
         if (n > std::size_t(-1) / sizeof(T)) {
             throw std::bad_alloc();
         }
-        memory_pool.offset += size_t(n * sizeof(T));
-        if (memory_pool.offset > memory_pool.size) {
+        memory_pool->offset += size_t(n * sizeof(T));
+        if (memory_pool->offset > memory_pool->size) {
             throw std::bad_alloc();
         }
-        void *foo = memory_pool.mem + memory_pool.offset;
+        void *foo = memory_pool->mem + memory_pool->offset;
         T *p = static_cast<T*>(foo);
         return p;
         throw std::bad_alloc();
@@ -48,19 +54,21 @@ bool operator==(const Mallocator<T>&, const Mallocator<U>&) { return true; }
 template <class T, class U>
 bool operator!=(const Mallocator<T>&, const Mallocator<U>&) { return false; }
 
+typedef Mallocator<string> string_allocator;
 
-typedef std::vector<std::string,Mallocator<string>> stringvec;
-
+typedef std::vector<std::string,string_allocator> string_vec;
 
 int main() {
+    
+    mempool memory_pool;
     
     memory_pool.mem = new char[4096];
     
     memory_pool.size = 4096;
     
-    memory_pool.offset += sizeof(stringvec);
+    memory_pool.offset += sizeof(string_vec);
     
-    stringvec *vec = new(memory_pool.mem) stringvec();
+    string_vec *vec = new(memory_pool.mem) string_vec((string_allocator(&memory_pool)));
     
     for (auto str : {"This", "is", "a", "test", "for", "a", "std::vector",
                      "with", "a", "custom", "allocator"})
@@ -73,7 +81,7 @@ int main() {
         cout << "Reading back string \"" << str << "\"" << endl;
     }
     
-    vec->~stringvec();
+    vec->~string_vec();
     
     delete[] memory_pool.mem;
     
